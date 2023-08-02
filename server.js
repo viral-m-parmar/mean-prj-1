5a6749a789f8023b963eec754b4d020d9710edac
viral parmar <parmarviral327@gmail.com>
2023-08-02 19:45:38 +0530

    cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express")
 require("./app/src/config/dbConfig").getDbConnection()
+const cors = require("cors")
 
 const app = express()
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use(cors());
 
 const catagoryRoutes = require("./app/src/routes/catagory.routes")
 const productRoutes = require("./app/src/routes/product.routes")
