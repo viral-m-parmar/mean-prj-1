22d2541252a505b646e8e6d8d9dbc574489bfbea
viral parmar <parmarviral327@gmail.com>
2023-08-13 00:29:26 +0530

    delete user

--- app/src/routes/public.routes.js
+++ app/src/routes/public.routes.js
@@ -6,7 +6,7 @@ const route = express.Router()
 route.post("/signupDb",sessionControllerDb.signup)
 route.post("/loginDb",sessionControllerDb.login)
 route.get("/getAllUsers",sessionControllerDb.getAllUsers)
-route.delete("/deleteUser",sessionControllerDb.deleteUserById)
+route.delete("/deleteUser/:userId",sessionControllerDb.deleteUserById)
 
 
 module.exports = route;
